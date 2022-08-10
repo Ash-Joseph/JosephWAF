d258922823fa1d85966dff1ee0af02704bcef7bf
Phan Anh Dũng <pad6521@gmail.com>
2022-08-10 07:42:58 +0700

    Add dependency installation script

---
+++ scripts/install_dependency.sh
@@ -0,0 +1,2 @@
+# Dependency for libModsecurity
+apt-get install git g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev
