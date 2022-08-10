3224d852318d1168aa9a2f4afaef9b70e1d97ab6
Phan Anh Dũng <pad6521@gmail.com>
2022-08-10 07:46:33 +0700

    Add /bin/bash to script

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # Dependency for libModsecurity
 apt-get install git g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev
